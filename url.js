bce9a8d8dbe34f508f8f2632fa2ed086226d7566
yufeng1268 <134466560+yufeng1268@users.noreply.github.com>
2024-03-28 15:43:15 +0800

    Update url.js

--- url.js
+++ url.js
@@ -10,10 +10,5 @@
 const urls = [
 	'https://forms.gle/LckQX5TZ1mwfsL7z8',
 	'https://forms.gle/SPZpeZLM44hmXCuM6',
-	'https://forms.gle/gpT28rZwijeDdFoG7',
-	'https://forms.gle/rGriBwZ6gfYCJ9i9A',
-	'https://forms.gle/NpHUVH87bPzjSfHx8',
-	'https://forms.gle/1BCHdAhbi2DZFrc16',
-	'https://forms.gle/xXhbXz8vkGdTJnDQ6',
-	'https://forms.gle/RgvZsBSfFHtHTJig7'
+	'https://forms.gle/rGriBwZ6gfYCJ9i9A'
 ];
